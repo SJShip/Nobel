6883efe3de414f1b70f4ca90faf550f4b0e34c55
Sergey Shipitsin <sergey.shipitsin@bluecieloecm.com>
2019-08-23 09:04:40 +0300

    Right Initial migration

---
+++ NobelApp.Data/Migrations/20190823055353_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NobelApp.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prizes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Year = table.Column<int>(nullable: false),
+                    Category = table.Column<int>(nullable: false),
+                    Motivation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prizes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Laureates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    BirthCity = table.Column<string>(nullable: true),
+                    BirthCountry = table.Column<string>(nullable: true),
+                    Sex = table.Column<int>(nullable: false),
+                    OrganisationId = table.Column<int>(nullable: true),
+                    PrizeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laureates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Laureates_Organizations_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Laureates_Prizes_PrizeId",
+                        column: x => x.PrizeId,
+                        principalTable: "Prizes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Laureates_OrganisationId",
+                table: "Laureates",
+                column: "OrganisationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Laureates_PrizeId",
+                table: "Laureates",
+                column: "PrizeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Laureates");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+
+            migrationBuilder.DropTable(
+                name: "Prizes");
+        }
+    }
+}
